5bd2b90045484fc5da1cc64c73400dc02c8a17ca
Benntobox <benjaminsboyle3@gmail.com>
2020-12-14 12:05:39 -0800

    Initial setup

---
+++ client/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(__dirname + './client/'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+  res.end();
+});
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
