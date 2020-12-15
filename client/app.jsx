5fc5e976a56a12215d9bee5bef41e4c2b60af467
Benntobox <benjaminsboyle3@gmail.com>
2020-12-15 01:24:04 -0800

    Nearing completion of setup

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import $ from 'jquery';
-
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {}
+    console.log("CREATED")
+    this.finish();
+  }
+
+  finish() {
+    $.ajax({
+      url: '/',
+      method: 'GET',
+      success: function() {
+        console.log('Got data')
+      }
+    })
+    console.log('FINISHED')
+  }
+
+  render() {
+    return (<div>TESTING IF IT WORKS MANG</div>)
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(express.static(__dirname + './client/'));
+app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
