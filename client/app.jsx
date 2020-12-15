9065e221094c9839d6cb496c15bf18a5450feff6
Benntobox <benjaminsboyle3@gmail.com>
2020-12-15 11:58:13 -0800

    Successfully got page to update to form 2 after form 1

--- client/app.jsx
+++ client/app.jsx
@@ -1,25 +1,83 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      step: 0
+    }
     console.log("CREATED")
     this.finish();
   }
 
-  finish() {
+  finish() { 
+    let app = this;
     $.ajax({
       url: '/',
       method: 'GET',
-      success: function() {
-        console.log('Got data')
+      success: function(data) {
+        console.log('Loaded')
       }
     })
-    console.log('FINISHED')
+  }
+
+  next() {
+    this.setState({step: this.state.step > 3 ? 0 : this.state.step + 1});
+  }
+
+  accountSubmit(e) {
+    let app = this;
+    e.preventDefault();
+    $.ajax({
+      url: '/account',
+      method: 'POST',
+      data: {name: e.target.value},
+      success: function (data) {
+        app.next();
+      }
+    })
   }
 
   render() {
-    return (<div>TESTING IF IT WORKS MANG</div>)
+    if (this.state.step === 0) { return (<F1 submit={this.accountSubmit.bind(this)}/>)}
+    if (this.state.step === 1) { return (<F2 />)}
+    if (this.state.step === 2) { return (<F3 />)}
+    if (this.state.step === 3) { return (<Summary />)}
   }
 }
+
+const F1 = (props) => (
+  <div>
+    <div>Enter Account Information</div>
+    <form onSubmit={props.submit.bind(this)}>
+      <label>Name:</label>
+      <input type="text" name="name"></input><br></br>
+      <label>Email:</label>
+      <input type="text" name="email"></input><br></br>
+      <label>Password:</label>
+      <input type="text" name="password"></input><br></br>
+      <input type="submit" name="Submit" value="submit"></input>
+    </form>
+  </div>
+)
+
+const F2 = (props) => (
+  <div>
+    <div>Enter Shipping Information:</div>
+    <form action="/shipping" method="post">
+      <label>Address:</label>
+      <input type="text" name="address1"></input><br></br>
+      <label>Address Cont:</label>
+      <input type="text" name="address2"></input><br></br>
+      <label>City:</label>
+      <input type="text" name="city"></input><br></br>
+      <label>State:</label>
+      <input type="text" name="state"></input><br></br>
+      <label>Zipcode:</label>
+      <input type="text" name="zipcode"></input><br></br>
+      <label>Phone Number:</label>
+      <input type="text" name="phone"></input><br></br>
+      <input type="submit" name="Submit" value="submit"></input>
+    </form>
+  </div>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- server.js
+++ server.js
@@ -8,9 +8,16 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
+app.post('/account', function (req, res) {
+  console.log("data", req.body);
+  //checkdatabase for email
+  //if not found, add user to user database
+  //
   res.end();
-});
+})
+
+// Get rid of favicon 404 error
+app.get('/favicon.ico', function (req, res) { res.end(); });
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
